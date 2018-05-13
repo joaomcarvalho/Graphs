cbf29d2b4011ace754f39fad6853a0046af8754c
João Pedro <joao.costa@ccc.ufcg.edu.br>
2018-05-13 18:40:00 -0300

    Metodo graphrep feito

--- GraphsAPI/src/implementation/GraphUtils.java
+++ GraphsAPI/src/implementation/GraphUtils.java
@@ -4,6 +4,7 @@ package implementation;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream.PutField;
 
 /**
  * aux entity to work with the graph
@@ -31,6 +32,45 @@ public class GraphUtils {
             graph.addEdge(j, i);
         }
         return graph;
+    }
+    
+    public static String graphRepresentation (Graph graph, String type) {
+    	if(type.equals("AM")) {
+    		String matrixRepresentation = putFirstLine(graph.getNumVertices());
+    		
+    		for(int i = 0; i < graph.getNumVertices(); i ++) {
+    			String line = "";
+    			for(int j = 0; j < graph.getNumVertices(); j++) {
+    				line += graph.getAdjacencyMatrix()[i][j] + " ";
+    			}
+    			matrixRepresentation += (i+1) + " " + line + "\n";
+    		}
+    		return matrixRepresentation;
+    	}
+    	else if(type.equals("AL")) {
+    		String alRep = "";
+    		for(int k = 0; k < graph.getNumVertices(); k++){
+    			String line = "";
+    			for (int l = 0; l < graph.getNumVertices(); l++) {
+    				if(graph.getAdjacencyMatrix()[k][l] != 0) {
+    					line += (l+1) + " ";
+    				}
+    				
+    			}
+    			alRep += (k+1) + "-" + line + "\n";
+    		}
+    		return alRep;
+    	}
+    	return "Invalid Type!";
+    }
+    
+    private static String putFirstLine(int size) {
+    	String fLine = " ";
+    	for (int i = 0; i < size; i++) {
+    		fLine += " " + (i+1);
+    	}
+    	fLine += "\n";
+    	return fLine;
     }
 
 
--- GraphsAPI/tests/implementationtest/GraphsTestSuite.java
+++ GraphsAPI/tests/implementationtest/GraphsTestSuite.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class GraphsTestSuite {
@@ -21,13 +22,23 @@ public class GraphsTestSuite {
 		try{
 
 			int [][] output =  {{0,1,0,0,1}, {1,0,0,0,1}, {0,0,0,0,1}, {0,0,0,0,1}, {1,1,1,1,0}};
-            graph = GraphUtils.readGraph("/home/severino/Documents/ufcg/Graphs/GraphsAPI/resources/input.txt");
+            graph = GraphUtils.readGraph("resources/input.txt");
             Assert.assertArrayEquals(output, graph.getAdjacencyMatrix());
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
+	
 
+	@Test
+	public void graphRepresentation() {
+		try {
+			graph = GraphUtils.readGraph("resources/input.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(GraphUtils.graphRepresentation(graph, "AM"));
+	}
 
 
 
