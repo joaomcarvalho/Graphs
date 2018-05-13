e625f2400adbebc3dccba5da7a576f5fcae739d7
João Pedro <joao.costa@ccc.ufcg.edu.br>
2018-05-13 11:28:13 -0300

    remoções

--- GraphsAPI/src/implementation/Graph.java
+++ GraphsAPI/src/implementation/Graph.java
@@ -1,5 +1,6 @@
 package implementation;
 
+import java.util.LinkedList;
 
 /**
  * This graph's representation take into account the follows rules:
@@ -46,6 +47,23 @@ public class Graph {
     public boolean hasEdge(int i, int j) {
 	    return this.adjacencyMatrix[i][j] >= DEFAULT_WEIGHT;
     }
+    
+
+	public void BFS(int v) {
+	    boolean visited[] = new boolean[adjacencyMatrix.length];
+	    LinkedList<Integer> queue = new LinkedList<Integer>();
+	    visited[v-1] = true;
+	    queue.add(v);
+	    while(queue.size()!=0) {
+	        int x = queue.remove();
+	        System.out.print(x + " ");
+	        for (int i=1; i <= adjacencyMatrix.length; i++) 
+	            if((adjacencyMatrix[x][i] == 1 && (!visited[i]))){
+	              queue.add(i);
+	              visited[i] = true;
+	             }
+	     }	
+	}
 
     public String toString(){
 		String out = "";
