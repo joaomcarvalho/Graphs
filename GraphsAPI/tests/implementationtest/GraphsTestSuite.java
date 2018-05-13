b48c0ca038b7bb9a91e700bd847b233d6329d355
João Pedro <joao.costa@ccc.ufcg.edu.br>
2018-05-13 10:55:19 -0300

    algumas coisas

--- GraphsAPI/tests/implementationtest/GraphsTestSuite.java
+++ GraphsAPI/tests/implementationtest/GraphsTestSuite.java
@@ -12,14 +12,8 @@ import java.net.URL;
 public class GraphsTestSuite {
 
 	Graph graph;
-
-
-
-
 	@Test
 	public void readGraph(){
-
-
 		try{
 
 			graph = GraphUtils.readGraph("/home/severino/Documents/ufcg/Graphs/GraphsAPI/resources/input.txt");
